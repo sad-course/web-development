99ee07c45a88d26b383e7381663df0b3c26ceb35
Mirla Lopes <mirlaisabel11@gmail.com>
2024-10-25 19:53:58 -0300

    adding templates and views

--- activity_01_django/poll/apps.py
+++ activity_01_django/poll/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EnqueteConfig(AppConfig):
+class PollConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'enquete'
+    name = 'poll'
